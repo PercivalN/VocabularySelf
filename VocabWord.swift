6e32856879512392900325fa0cd7a079327c5d1d
Percy Ngan <percival.ngan@gmail.com>
2019-08-12 22:40:07 -0500

    1st commit

---
+++ VocabWord.swift
@@ -0,0 +1,14 @@
+//
+//  VocabWord.swift
+//  VocabularySelf
+//
+//  Created by Percy Ngan on 8/12/19.
+//  Copyright © 2019 Lamdba School. All rights reserved.
+//
+
+import Foundation
+
+struct VocabWord {
+	var word: String
+	var definition: String
+}
